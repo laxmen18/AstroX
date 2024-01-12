2b57d6ecd04fa90d034d5f6700319116bf353adb
Laxmen <68938953+laxmen18@users.noreply.github.com>
2024-01-12 21:30:51 +0530

    Add files via upload

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Built by Laxmen
+          <span>
+            <Link
+              style={{ color: "white" }}
+              className="nav-link"
+              to={"https://www.linkedin.com/in/laxmen-murali-/"}
+            >
+            </Link>
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
